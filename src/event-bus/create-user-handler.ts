29a3e4515fd52ab2e11321ec82d14da7961b35b2
SamuelHVRodrigues <samuelhvrodrigues@gmail.com>
2022-11-08 15:09:08 -0300

    Add route createUser

--- src/event-bus/create-user-handler.ts
+++ src/event-bus/create-user-handler.ts
@@ -3,9 +3,7 @@ import { session } from '../neo4j';
 import { CreateUserCommand } from './create-user-command';
 
 export const createUserHandler = handlerFor(CreateUserCommand, async (command) => {
-  console.log('command.userCreationParams: ');
-  console.log(command.userCreationParams);
-  await session.run('CREATE (:USER {name: $name, email: $email, password: $password}', {
+  await session.run(`CREATE (user:USER {name: $name, email: $email, password: $password})`, {
     name: command.userCreationParams.name,
     email: command.userCreationParams.email,
     password: command.userCreationParams.password,
--- src/neo4j.ts
+++ src/neo4j.ts
@@ -5,17 +5,3 @@ const driver = neo4j.driver(
   neo4j.auth.basic(process.env.NEO4J_USER, process.env.NEO4J_PASSWORD),
 );
 export const session = driver.session();
-
-// export async function test() {
-//   try {
-//     const result = await session.run(`CREATE (user:USER {name: "Jane Doe"}) RETURN user`);
-//     const singleRecord = result.records[0];
-//     const node = singleRecord.get(0);
-
-//     console.log(node.properties.name);
-//   } finally {
-//     await session.close();
-//   }
-
-//   await driver.close();
-// }
