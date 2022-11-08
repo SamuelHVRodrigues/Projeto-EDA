1cd062c5d15259c43185eb68c981ab04f6873ba2
SamuelHVRodrigues <samuelhvrodrigues@gmail.com>
2022-11-08 09:50:32 -0300

    Add neo4j

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,9 @@
+import * as dotenv from 'dotenv';
+dotenv.config({ path: './.env' });
 import express, { json, urlencoded } from 'express';
 import { RegisterRoutes } from '../build/routes';
 import { startBus } from './event-bus/event-bus';
+import { test } from './neo4j';
 
 export const app = express();
 
@@ -9,3 +12,4 @@ app.use(json());
 
 RegisterRoutes(app);
 startBus();
+test();
